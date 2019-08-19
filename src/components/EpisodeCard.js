fea4c6c8de00e76703be9908b44dd61d3105b821
Dave Kane <davelkane@gmail.com>
2019-08-18 21:34:55 -0500

    EpisodeCard expanded, Part 1 complete

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
-import { Card } from 'semantic-ui-react'
+import { Card, Label, Icon } from 'semantic-ui-react'
 
 const EpisodeCard = (props) => {
 
   const { results } = props
 
-  console.log(results)
+  const meta = (
+    <p>
+      <Icon name='calendar alternate outline' />
+      {results.air_date}
+    </p>
+  )
+
+  const extra = (
+    <Label>
+      <Icon name="users" />
+      {`Characters: ${results.characters.length}`}
+    </Label>
+  )
 
   return (
     <Card
       header={results.name}
+      meta={meta}
+      extra={extra}
       id={results.id}
     />
   )
