5f256ba1cc4b89c4d5e79dfb408c2127a971aaf8
Dave Kane <davelkane@gmail.com>
2019-08-18 21:51:32 -0500

    Characters routed

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import WelcomePage from './WelcomePage';
+import Characters from './CharacterList';
+
 
 const AppRouter = () => {
 
   return (
     <>
       <Route exact path="/" component={WelcomePage} />
+      <Route
+        exact path="/characters"
+        render={props => <Characters
+          {...props}
+        />}
+      />
     </>
   )
 }
