37d205d63aac878d547f55fb381d649cdf43e837
Dave Kane <davelkane@gmail.com>
2019-08-18 20:34:39 -0500

    TabNav fleshed out, not wired up

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -3,6 +3,30 @@ import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
-export default function TabNav() {
+const TabNav = () => {
+
+  return (
+    <Menu>
+      <Menu.Item >
+        <Icon name='home' />
+        Home
+      </Menu.Item>
+      <Menu.Item>
+        <Icon name='users' />
+        Characters
+      </Menu.Item>
+      <Menu.Item>
+        <Icon name='map signs' />
+        Locations
+      </Menu.Item>
+      <Menu.Item>
+        <Icon name='film' />
+        Episodes
+      </Menu.Item>
+    </Menu>
+  )
+
 
 };
+
+export default TabNav
