cd48f9aedf6628c78d4b2e895649a772b89f22b9
Dave Kane <davelkane@gmail.com>
2019-08-18 21:25:36 -0500

    LocationCard expanded on

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
-import { Card } from 'semantic-ui-react'
+import { Card, Label, Icon } from 'semantic-ui-react'
 
 const LocationCard = (props) => {
 
   const { results } = props
 
-  console.log(results)
+  const extra = (
+    <Label>
+      <Icon name='users' />
+      {`Residents: ${results.residents.length}`}
+    </Label>
+  )
 
   return (
     <Card
       header={results.name}
+      meta={results.type}
+      description={`Dimension: ${results.dimension}`}
+      extra={extra}
       id={results.id}
     />
   )
